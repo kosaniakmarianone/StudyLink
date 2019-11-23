74b12bc68cf1769a6dc5b4e493a334f3153cb054
Marik1 <kosaniakm@gmail.com>
2019-11-23 23:39:54 +0200

    Updates

---
+++ precache-manifest.1612bb74dba45cde451c5786cb00e279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e47cb62ef2aa3a28e7881de4ad305ac",
+    "url": "/StudyLink/index.html"
+  },
+  {
+    "revision": "0c11c3c6fda185c18923",
+    "url": "/StudyLink/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "b0c26830eef2d038a40d",
+    "url": "/StudyLink/static/css/main.74dc4d08.chunk.css"
+  },
+  {
+    "revision": "0c11c3c6fda185c18923",
+    "url": "/StudyLink/static/js/2.85ea29f4.chunk.js"
+  },
+  {
+    "revision": "b0c26830eef2d038a40d",
+    "url": "/StudyLink/static/js/main.6759be10.chunk.js"
+  },
+  {
+    "revision": "2ac25a469ce14be202c2",
+    "url": "/StudyLink/static/js/runtime-main.0ca4e564.js"
+  },
+  {
+    "revision": "09ba2146572699509a685bade9a8a398",
+    "url": "/StudyLink/static/media/program.09ba2146.jpg"
+  }
+]);
